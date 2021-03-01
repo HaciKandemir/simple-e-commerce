4e5d045e4a0b61881e1046856beaa3b4231ee31e
HaciKandemir <hacikandemir2000@hotmail.com>
2021-03-01 20:56:07 +0300

    Ürün sergileme kısmı tamamlandı. Sayfa statik olarak örneği tamamlandı

--- View/index.php
+++ View/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="View/assets/css/style.css?v=1">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
@@ -32,6 +34,81 @@
                 </div>
             </div>
         </header>
+
+        <div class="content">
+            <div class="row">
+                <div class="col-sm-3 product-item">
+                    <div class="card">
+                        <img class="card-img-top" src="https://productimages.hepsiburada.net/s/21/280-413/9937404297266.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="prdct-name">Iphone XS Max</span>
+                            </h5>
+                            <p class="card-text">
+                                <span class="prdct-price">13.199 TL</span>
+                            </p>
+                            <a href="#" class="btn add-to-basket">Sepete Ekle</a>
+                        </div>
+                    </div>                   
+                </div>
+                <div class="col-sm-3 product-item">
+                    <div class="card">
+                        <img class="card-img-top" src="https://www.whiskas.com/Content/images/products/large/meaty-selections.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="prdct-name">Wiskas Cat Feed</span>
+                            </h5>
+                            <p class="card-text">
+                                <span class="prdct-price">250 TL</span>
+                            </p>
+                            <a href="#" class="btn add-to-basket">Sepete Ekle</a>
+                        </div>
+                    </div>                   
+                </div>
+                <div class="col-sm-3 product-item">
+                    <div class="card">
+                        <img class="card-img-top" src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_240/public/-DryTurkey-and-Chicken400x500.png?itok=2kD7dIRy" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="prdct-name">Bella Natural Dry Small Dog Food</span>
+                            </h5>
+                            <p class="card-text">
+                                <span class="prdct-price">125,75 TL</span>
+                            </p>
+                            <a href="#" class="btn add-to-basket">Sepete Ekle</a>
+                        </div>
+                    </div>                   
+                </div>
+                <div class="col-sm-3 product-item">
+                    <div class="card">
+                        <img class="card-img-top" src="https://www.whiskas.com/Content/images/products/large/meaty-selections.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="prdct-name">Wiskas Cat Feed</span>
+                            </h5>
+                            <p class="card-text">
+                                <span class="prdct-price">250 TL</span>
+                            </p>
+                            <a href="#" class="btn add-to-basket">Sepete Ekle</a>
+                        </div>
+                    </div>                   
+                </div>
+                <div class="col-sm-3 product-item">
+                    <div class="card">
+                        <img class="card-img-top" src="https://productimages.hepsiburada.net/s/21/280-413/9937404297266.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <span class="prdct-name">Iphone XS Max</span>
+                            </h5>
+                            <p class="card-text">
+                                <span class="prdct-price">13.199 TL</span>
+                            </p>
+                            <a href="#" class="btn add-to-basket">Sepete Ekle</a>
+                        </div>
+                    </div>                   
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
